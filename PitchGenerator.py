4c6700317ce2c734461df67e95ca89b544d052f3
Jordan Hoeber <jordan.hoeber@gmail.com>
2015-08-06 21:02:28 -0400

    Now reading lines of notes and playing them. Problem occuring when notes' values are too short, for example a 1/16th note at 200 BPM will not play

--- PitchGenerator.py
+++ PitchGenerator.py
@@ -5,17 +5,17 @@ from pyaudio import PyAudio
 class PitchGenerator(object):
     def __init__(self, beatsPerMinute = 60, timeSignature = "4/4", key = "c"):
             
-        self.BPM = beatsPerMinute
+        self.beatsPerMin = beatsPerMinute
         self.timeSig = timeSignature.split('/')
-        beatLength = 60 / beatsPerMinute
         self.key = key
         self.bitRate = 16000
         self.generateNoteDictionary()
-        self.noteValueDict = { 'w' : beatLength * 4,
-                               'h' : beatLength * 2,
-                               'q' : beatLength,
-                               'e' : beatLength / 2,
-                               's' : beatLength / 4 }
+        beatLength = 60 / float(self.beatsPerMin)
+        self.noteValueDict = { 'W' : beatLength * 4.0,
+                               'H' : beatLength * 2.0,
+                               'Q' : beatLength,
+                               'E' : beatLength / 2.0,
+                               'S' : beatLength / 4.0 }
 
 
     def generateNoteDictionary(self):
@@ -33,6 +33,7 @@ class PitchGenerator(object):
                          9  : ['F#', 'Gb'],
                          10 : ['G'],
                          11 : ['G#', 'Ab']  }
+        self.noteDict = { 'r'  : -1 }
         self.noteDict = { 'A0' : initFreq }
         for i in range(1,88):
             tempKeyNotes = keyNoteDict[i%12]
@@ -43,17 +44,35 @@ class PitchGenerator(object):
                     self.noteDict[tempKeyNotes[j] + str(int(math.floor((i - 3) / 12) + 1))] =\
                             (initFreq * (2.0 ** (1.0/12.0)) ** i)
 
+    def playLine(self, line):
+        parts = line.split(" ")
+        parts.reverse()
+        while parts:
+            note = parts.pop()
+            value = parts.pop()
+            if not (note in self.noteDict):
+                print "Poorly Formated Line try again!\n"
+                return
+            if not (value in self.noteValueDict):
+                print "Poorly Formated Line try again!\n"
+                return
+            waveData = self.generateWaveData(note, value)
+            self.playWaveData(waveData)
+
+
+
     def generateWaveData(self, note, noteValue):
         noteLen = self.noteValueDict[noteValue]
-        noteFreq = self.noteDict[note]
         numFrames = int(self.bitRate * noteLen)
         restFrames = numFrames % self.bitRate
         waveData = ''
-        for x in xrange(numFrames):
-            waveData = waveData + chr(int(math.sin(x / ((self.bitRate / noteFreq) / math.pi)) *127 + 128))
-        for x in xrange(restFrames):
-            waveData = waveData + chr(128)
-
+        if note == 'r':
+            for x in xrange(numFrames):
+                waveData = waveData + chr(128)
+        else:
+            noteFreq = self.noteDict[note]
+            for x in xrange(numFrames):
+                waveData = waveData + chr(int(math.sin(x / ((self.bitRate / noteFreq) / math.pi)) *127 + 128))
         return waveData
 
     def playWaveData(self, waveData):
--- terminalComposer.py
+++ terminalComposer.py
@@ -2,21 +2,24 @@ import math
 import pyaudio
 from PitchGenerator import PitchGenerator
 
-x = PitchGenerator()
-waveD = x.generateWaveData("C5", 'q')
-x.playWaveData(waveD)
-waveD = x.generateWaveData("D5", 'q')
-x.playWaveData(waveD)
-waveD = x.generateWaveData("E5", 'q')
-x.playWaveData(waveD)
-waveD = x.generateWaveData("F5", 'q')
-x.playWaveData(waveD)
-waveD = x.generateWaveData("G5", 'q')
-x.playWaveData(waveD)
-waveD = x.generateWaveData("A5", 'q')
-x.playWaveData(waveD)
-waveD = x.generateWaveData("B5", 'q')
-x.playWaveData(waveD)
-waveD = x.generateWaveData("C6", 'q')
-x.playWaveData(waveD)
+print "\n\n\n\n\n\n\n\n\n\nWelcome to Terminal Composer!\n" \
+        "Enter keys of the keyboard, a space, then the first leter of the note value\n" \
+        "For example: D4 Q F#4 Q A4 Q would play an arpeggiated D major chord starting on the\n" \
+        "D after middle C\nType 'exit' to close\n"
+BPM = input("First enter a tempo (in BPM) between 40 and 200   ")
+
+#timeSig = raw_input("Enter a time signature (4/4, 6/8, 3/4, etc.)  ")
+#while not(BPM.is_integer()):
+#    BPM = input("Enter new tempo within 40 and 200   ")
+while (BPM > 200) or (BPM < 40):
+    BPM = input("Enter new tempo within 40 and 200   ")
+
+pitchGen = PitchGenerator(int(BPM))#, timeSig)
+while True:
+    line = raw_input("Enter line of notes to play\n")
+    line = line.upper()
+    if line == "EXIT":
+        quit()
+    pitchGen.playLine(line)
+
 
